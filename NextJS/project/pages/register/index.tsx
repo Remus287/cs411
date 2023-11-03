42e424a86197f71465f4bafc526ce7aa541e187e
rlaziz <rlaziz@bu.edu>
2023-11-03 17:49:43 -0400

    NextJS implementation

---
+++ NextJS/project/pages/register/index.tsx
@@ -0,0 +1,33 @@
+import Head from 'next/head'
+import clientPromise from '../../lib/mongodb'
+import type { InferGetServerSidePropsType, GetServerSideProps } from 'next'
+
+type ConnectionStatus = {
+	isConnected: boolean
+}
+
+export const getServerSideProps: GetServerSideProps<
+	ConnectionStatus
+> = async () => {
+	try {
+		await clientPromise
+		return {
+			props: { isConnected: true },
+		}
+	} catch (e) {
+		console.error(e)
+		return {
+			props: { isConnected: false },
+		}
+	}
+}
+
+export default function Home({
+	isConnected,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+	return (
+		<div>
+			<h1>Bluh Bluh</h1>
+		</div>
+	)
+}

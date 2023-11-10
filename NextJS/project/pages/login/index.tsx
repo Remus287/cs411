9e8ca036511a3843ad1e554090699c71b93a5380
rlaziz <rlaziz@bu.edu>
2023-11-10 09:15:41 -0500

    Added simple login functionality

--- NextJS/project/pages/login/index.tsx
+++ NextJS/project/pages/login/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import clientPromise from '../../lib/mongodb'
 import type { InferGetServerSidePropsType, GetServerSideProps } from 'next'
+import {useRouter} from "next/router";
 
 type ConnectionStatus = {
 	isConnected: boolean
@@ -25,6 +26,32 @@ export const getServerSideProps: GetServerSideProps<
 export default function Home({
 	isConnected,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+	const router = useRouter ();
+	const handleSubmit  = (e) => {
+		e.preventDefault ()
+		let username = e.target[0].value
+		let password = e.target[1].value
+		
+		const user = {
+			username: username, password: password,
+		}
+		
+		fetch ('/api/auth/login', {
+			method: 'POST', headers: {
+				'Content-Type': 'application/json',
+			}, body: JSON.stringify (user),
+		})
+			.then ((res) => {
+				console.log (res.status)
+				if (res.status === 200) {
+					router.push ('/feed')
+				}
+				return res.json ()
+			})
+			.then ((data) => {
+				console.log (data)
+			}).catch ((err) => console.log (err))
+	}
 	return (
 		<div className={'w-full h-full flex justify-start'} style={{
 			backgroundImage: 'url(./assets/background.jpg)',
@@ -33,9 +60,9 @@ export default function Home({
 			backgroundRepeat: 'no-repeat',
 			
 		}}>
-			<section className={"w-1/3 h-full flex py-32 px-20 bg-white flex-col gap-10"}>
+			<section className={"w-1/3 h-full flex py-20 px-20 bg-white flex-col gap-10"}>
 				<h1 className={'text-4xl font-medium border-b-4 border-blue-700 py-4'}>Log In</h1>
-				<form className={'flex flex-col w-full gap-4 items-end'}>
+				<form className={'flex flex-col w-full gap-4 items-end'} onSubmit={(e)=>{handleSubmit(e)}}>
 					<label className={'w-full flex flex-col gap-1'}>
 						<label className={'text-gray-700 text-lg pl-2'}>Username</label>
 						<input type={'text'} className={'px-4 py-2 bg-white/[.9] outline outline-1 outline-gray-300 w-full'} />

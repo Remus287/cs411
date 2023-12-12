d1aef9705d429117d7403340e40c09f9afae508a
Remus Harris <remus@bu.edu>
2023-12-12 18:42:08 -0500

    final checkpoint and api example changes

--- JavascriptLogicCode/stocknewsapi.js
+++ JavascriptLogicCode/stocknewsapi.js
@@ -21,7 +21,7 @@ DO NOT PUSH THE KEY!!!!
 async function getStockNewsData(stockSymbol) {
   const apiKey = require('./key');
   
-  const endpoint = `https://stocknewsapi.com/api/v1?tickers=${stockSymbol}&items=3&page=1&sortby=rank&extra-fields=rankscore&token=${apiKey}`;
+  const endpoint = `https://stocknewsapi.com/api/v1?tickers=${stockSymbol}&items=100&page=1&sortby=rank&extra-fields=rankscore&token=${apiKey}`;
   const endpointResponse = await fetch(endpoint);
   const endpointData = await endpointResponse.json();
 
@@ -31,39 +31,39 @@ async function getStockNewsData(stockSymbol) {
 
 async function getSentimentAndData(stockSymbol) {
 
-    const result = await getStockNewsData(stockSymbol);
-    const newsData = result.data.data;
+  const result = await getStockNewsData(stockSymbol);
+  const newsData = result.data.data;
 
-    // Define a mapping for sentiment values
-    const sentimentMapping = {
-    'Positive': 1,
-    'Neutral': 0,
-    'Negative': -1
-    };
+  // Define a mapping for sentiment values
+  const sentimentMapping = {
+  'Positive': 1,
+  'Neutral': 0,
+  'Negative': -1
+  };
 
-    // Calculate the average sentiment score
-    const totalSentiment = newsData.reduce((sum, article) => {
-    const sentimentValue = sentimentMapping[article.sentiment] || 0;
-    return sum + sentimentValue;
-    }, 0);
+  // Calculate the average sentiment score
+  const totalSentiment = newsData.reduce((sum, article) => {
+  const sentimentValue = sentimentMapping[article.sentiment] || 0;
+  return sum + sentimentValue;
+  }, 0);
 
-    const averageSentiment = totalSentiment / newsData.length;
+  const averageSentiment = totalSentiment / newsData.length;
 
-    // Return an object containing both average sentiment and original news data
-    return {averageSentiment, newsData};
+  // Return an object containing both average sentiment and original news data
+  return {averageSentiment, newsData};
 }
   
 
 
 
-  // Example usage
-  const stockSymbol = 'AAPL';
-  
-  getSentimentAndData(stockSymbol)
-    .then(result => {
-      console.log('Average Sentiment:', result.averageSentiment);
-      console.log('News Data:', result.newsData);
-    })
-    .catch(error => {
-      console.error('Error:', error);
-    });
+// Example usage
+const stockSymbol = 'AAPL';
+
+getSentimentAndData(stockSymbol)
+  .then(result => {
+    console.log('News Data:', result.newsData);
+    console.log('Average Sentiment:', result.averageSentiment);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
